6a10928cff4410749f256ef035ff6e62edd620e4
Susan Githaiga <susangithaiga26@gmail.com>
2025-04-16 07:45:19 +0300

    update ui

---
+++ src/views/Attribution.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div
+      className="attributionflex flex-row justify-center  text-sm pt-12 pb-6"
+    >
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" className="pl-1.5"
+        >Frontend Mentor</a
+      >. Coded by
+      <a href="https://github.com/SusanGithaigaN">
+        <span className="pl-1.5">Susan Githaiga</span></a
+      >.
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.attribution {
+  font-size: 11px;
+  text-align: center;
+}
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
